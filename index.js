c0d69e356ad6b3582da578cd0e2a99942289ed56
David Behar <davbelom@gmail.com>
2019-10-22 19:21:11 -0500

    error middlewareswith boom

--- index.js
+++ index.js
@@ -2,18 +2,24 @@ const express = require('express');
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies');
 const yearApi = require('./routes/year');
-const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers');
+const { logErrors, errorHandler, wrapErrors } = require('./utils/middleware/errorHandlers');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 const app = express();
 
 // body parser middleware
 app.use(express.json());
 
+//routes
 yearApi(app);
 moviesApi(app);
 
+// Catch 404
+app.use(notFoundHandler);
+
 //Error middleware
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, () => {
--- utils/middleware/errorHandlers.js
+++ utils/middleware/errorHandlers.js
@@ -1,3 +1,4 @@
+const boom = require('@hapi/boom');
 const { config } = require('../../config');
 
 console.log('Developer state', config.dev);//eslint-disable-line no-console
@@ -5,7 +6,7 @@ console.log('Developer state', config.dev);//eslint-disable-line no-console
 function withErrorStack(error, stack) {
   if (config.dev) {
     console.log('dev = true');//eslint-disable-line no-console
-    return { error, stack };
+    return { ...error, stack };
   }
   return error;
 }
@@ -15,12 +16,21 @@ function logErrors(err, req, res, next) {
   next();
 }
 
+function wrapErrors(err, req, res, next) {
+  if (!err.isBoom) {
+    next(boom.badImplementation(err));
+  }
+  next(err);
+}
+
 function errorHandler(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json(withErrorStack(err.message, err.stack));
+  const { output: { statusCode, payload } } = err;
+  res.status(statusCode);
+  res.json(withErrorStack(payload, err.stack));
 }
 
 module.exports = {
   logErrors,
+  wrapErrors,
   errorHandler,
 };
